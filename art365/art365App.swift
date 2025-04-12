e427db57f5a5e77bd47bd3781a8ce1647d751121
nozomemein <b8yukifsukeo999n@gmail.com>
2025-04-12 17:43:52 +0900

    ✨init repo

---
+++ art365/art365App.swift
@@ -0,0 +1,32 @@
+//
+//  art365App.swift
+//  art365
+//
+//  Created by 土方希 on 2025/04/12.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct art365App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ art365Tests/art365Tests.swift
@@ -0,0 +1,17 @@
+//
+//  art365Tests.swift
+//  art365Tests
+//
+//  Created by 土方希 on 2025/04/12.
+//
+
+import Testing
+@testable import art365
+
+struct art365Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
